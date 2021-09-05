d260ef74f9da1a0b8a36218b0d7c82c4ce9c8046
Byeonjinha <jinhaday@gamil.com>
2021-09-05 19:23:24 +0900

    2021/09/05

---
+++ Ecote/3-2 (그리디 큰 수의 법칙).py
@@ -0,0 +1,25 @@
+import sys
+n,m,k = map(int, sys.stdin.readline().split())
+data=list(map(int,input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+count = int(m/k+1)*k
+count += m%(k+1)
+result =0
+result += (count)*first
+result += (m-count) * second
+print(result)
+# result = 0
+# while True:
+#     for i in range(k):
+#         if m == 0 :
+#             break
+#         result +=first
+#         m -= 1
+#     if m ==0:
+#         break
+#     result += second
+#     m -= 1
+# print(result)
