17269f6943845696fe1465d83c397099e0f82158
Jinha <jinhaday@gmail.com>
2021-12-28 22:47:14 +0900

    2021/12/28 cospro 1-4 문제풀이

--- COS Pro/1급 파이썬 문제/1차 1급 3_initial_code.py
+++ COS Pro/1급 파이썬 문제/1차 1급 3_initial_code.py
@@ -5,21 +5,21 @@ def func_a(numA, numB, exp):
         return numA - numB
     elif exp == '*':
         return numA * numB
-    
+#연산자의 위치를 반환 , enumeerate 는 index와 값을 튜플형태로 반환
 def func_b(exp):
     for index, value in enumerate(exp):
         if value == '+' or value == '-' or value == '*':
             return index
-        
+#두개의 숫자를 반환
 def func_c(exp, idx):
     numA = int(exp[:idx])
     numB = int(exp[idx + 1:])
     return numA, numB
 
 def solution(expression):
-    exp_index = func_@@@(@@@)
-    first_num, second_num = func_@@@(@@@)
-    result = func_@@@(@@@)
+    exp_index = func_b(expression)
+    first_num, second_num = func_c(expression, exp_index) #name unpack
+    result = func_a(first_num,second_num,expression[exp_index])
     return result
 
 #The following is code to output testcase.
--- COS Pro/1급 파이썬 문제/1차 1급 4_initial_code.py
+++ COS Pro/1급 파이썬 문제/1차 1급 4_initial_code.py
@@ -3,15 +3,26 @@
 
 def solution(num):
     num += 1
-    digit = 1
-    while num // digit % 10 == 0:
-        num += digit
-        digit *= 10
-    return num
+    #for statemenet
+    # answer = ''
+    # for x in str(num):
+    #     if x == '0' : x = '1'
+    #     answer +=x
+    # return answer
+    #comprehension (expression)
+    answer = ''.join('1' if x == '0' else x for x in str(num))
+    return answer
+
+
+    # digit = 1
+    # while num // digit % 10 == 0:
+    #     num += digit
+    #     digit *= 10
+    # return num
 
 #The following is code to output testcase.
-num = 9949989;
-ret = solution(num)
+num = 9949999;
+ret = solution(num  )
 
 #Press Run button to receive output. 
 print("Solution: return value of the function is", ret, ".")
