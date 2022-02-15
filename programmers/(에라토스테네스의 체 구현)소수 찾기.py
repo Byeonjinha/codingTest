47a6732bb9f2fd31dfb23acff0a71875ada01e17
Jinha <jinhaday@gmail.com>
2022-02-15 11:01:59 +0900

    2022/02/15

--- programmers/(에라토스테네스의 체 구현)소수 찾기.py
+++ programmers/(에라토스테네스의 체 구현)소수 찾기.py
@@ -1,5 +1,5 @@
 
-def solution(n):
+def sosu(n):
     a = [True] * (n + 1)
     m = int(n**0.5)
     for i in range(2, m + 1):
@@ -7,9 +7,19 @@ def solution(n):
             for j in range(i + i, n + 1, i):
                 a[j] = False
     answer =(len([i for i in range(2, n + 1) if a[i] == True]))
+    print(a)
     return answer
 
 '''
 소수 개수를 return 함
 len 을 빼면 소수 list return 함
+
+=
 '''
+
+def solution(n):
+    sosu(n)
+    return 0
+
+n = 10
+solution(n)
--- programmers/실패율.py
+++ programmers/실패율.py
@@ -1,38 +1,31 @@
-import copy
-# N= 4
-# stages=	[4,4,4,4,4]
-# N= 5
-# stages=	[2, 1, 2, 6, 2, 4, 3, 3]
-# N= 6
-# stages=	[1, 1, 1, 1, 2, 2, 2, 2]
-
-
-silpae=[]
-silpae2=[]
-
-
 def solution(N, stages):
-    clear = [0 for _ in range(N + 1)]
-    answer =[]
-    for i in stages:
-        clear[i-1]+=1
-    for i  in range(0,len(clear)-1):
-        if sum(clear[i::])==0:
-            silpae.append(0)
+    dic = {}
+    list_1 = []
+    for i in range(len(stages)):
+        if stages[i] not in dic:
+            dic[stages[i]]=1
         else:
-            silpae.append(clear[i]/sum(clear[i::]))
+            dic[stages[i]]+=1
+    boonmo=0
+    for i in range(1,N+1):
+        for j in dic:
+            if i<= j :
+                boonmo+= dic.get(j)
+        boonja = dic.get(i)
+        if boonja is None:
+            boonja=0
+        if boonmo==0:
+            list_1.append([i,0])
+        else:
+          list_1.append([i,boonja/boonmo])
+        boonmo=0
+    answer = []
+    list_1.sort(key=lambda x : (-x[1], x[0]))
+    for i in range(len(list_1)):
+        answer.append(list_1[i][0])
 
-    # if max(stages)-N== 1:
-    #     del silpae[-1]
-    silpae2 = copy.deepcopy(silpae)
-    silpae2.sort(reverse=True)
-    for i in silpae2:
-        answer.append(silpae.index(i)+1)
-        silpae[silpae.index(i)]=''
     print(answer)
     return answer
+N = 4
+stages = [4,4,4,4,4]
 solution(N, stages)
-
-
-
-
