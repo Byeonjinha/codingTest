3837de95308d7b202d94f73492324453de7d1e4d
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-02-14 16:57:36 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,11 +1,14 @@
-A, B = list(map(int, input().split()))
-if B > A :
-    A, B = B, A
-for i in range(A,0,-1):
-    if A%i == 0 and B%i == 0:
-        print(i)
-        break
-for j in range(A, A*B+1):
-    if j%A == 0 and j%B == 0:
-        print(j)
-        break
+import sys
+
+
+def gcd(x: int, y: int) -> int:
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x % y)
+
+
+x, y, = map(int, sys.stdin.readline().strip().split())
+gcd = gcd(x, y)
+print(gcd)
+print(x * y // gcd)
