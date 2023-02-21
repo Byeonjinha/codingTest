b234378799b945f3ece437a4a6fd8f383c5d0699
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-02-21 17:35:58 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,14 +1,12 @@
 from collections import deque
 def solution(cards1, cards2, goal):
     cards1, cards2, goal = deque(cards1), deque(cards2), deque(goal)
-    flag = True 
+    flag = True
     while (cards1 or cards2) and goal and flag:
-        word = goal.popleft()
-        flag = False
+        word, flag = goal.popleft(), False
         if len(cards1) != 0:
             if word == cards1[0]: cards1.popleft(); flag = True; word = ""
         if len(cards2) != 0:
             if word == cards2[0]: cards2.popleft(); flag = True; word = ""
-    if goal or word != "":
-        return "No"
+    if goal or word != "": return "No"
     return "Yes"
