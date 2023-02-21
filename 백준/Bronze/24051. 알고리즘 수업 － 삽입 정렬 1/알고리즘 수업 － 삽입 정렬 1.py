c4a241f5c4f48b18cb4c59a92310f4b2833ceb46
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-02-21 21:03:22 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 260 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.py
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.py
@@ -8,18 +8,18 @@ for i in range(1, n):
         break
     j = i
     tmp = a[i]
-    flag2 = False
+    flag2 = False #대소관계가 정렬되어 있는지 확인
     while j > 0 and a[j - 1] > tmp:
-        flag2 = True
-        k -= 1
+        flag2 = True  #대소관계가 정렬되어 있는지 확인 했을 때 정렬 되어있지 않은 경우
+        k -= 1  #대소관계가 정렬되어 있는지 확인 했을 때 정렬 되어있지 않은 경우 변화했기 때문에 k - 1
         a[j] = a[j - 1]
-        if k <= 0:
+        if k == 0:
             flag1 = False
             break
         j -= 1
     if k <= 0:
         break
-    if flag2:
+    if flag2: #대소관계가 정렬되어 있는지 확인 했을 때 정렬 되어있지 않은 경우 재정렬한 k - 1
         k -= 1
     a[j] = tmp
 if k == 0:
