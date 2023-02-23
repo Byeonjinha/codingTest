b612529c7c3ef5f38f8ef864b8910de35921e132
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-02-23 12:31:02 +0900

    [Silver V] Title: 알고리즘 수업 - 힙 정렬 1, Time: 1200 ms, Memory: 210924 KB -BaekjoonHub

--- 백준/Silver/24173. 알고리즘 수업 － 힙 정렬 1/알고리즘 수업 － 힙 정렬 1.py
+++ 백준/Silver/24173. 알고리즘 수업 － 힙 정렬 1/알고리즘 수업 － 힙 정렬 1.py
@@ -1,37 +1,44 @@
 import sys
+
 sys.setrecursionlimit(10 ** 4)
 n, k = map(int, sys.stdin.readline().strip().split())
 a = list(map(int, sys.stdin.readline().strip().split()))
 flag = True
 
-def heapify(array,parent,size):
+
+def heapify(array, parent, size):
     global k, flag
     if k == 0:
         flag = False
         return
     smallest = parent
-    left=(2*parent)+1
-    right=(2*parent)+2
+    left = (2 * parent) + 1
+    right = (2 * parent) + 2
 
-    if (left<size) and (array[left]< array[smallest]): smallest=left
-    if (right<size) and (array[right]<array[smallest]): smallest=right
-    if smallest!=parent:
+    if (left < size) and (array[left] < array[smallest]): smallest = left
+    if (right < size) and (array[right] < array[smallest]): smallest = right
+    if smallest != parent:
         k -= 1
-        
+
         if k == 0:
             flag = False
             print(min(array[smallest], array[parent]), max(array[smallest], array[parent]))
             return
-        array[parent],array[smallest]=array[smallest],array[parent]
-        heapify(array,smallest,size)
+        array[parent], array[smallest] = array[smallest], array[parent]
+        heapify(array, smallest, size)
+
+
+def build_min_heap(a, n):
+    for i in range((n // 2) - 1, -1, -1):
+        heapify(a, i, n)
+
 
 def heap_sort(a):
     global n, k, flag
-    if k == 0:
+    if k == 0:  # k 가 0이면 정렬 종료.
         flag = False
         return
-    for i in range((n // 2) - 1, -1, -1):
-        heapify(a, i, n)
+    build_min_heap(a,n)
 
     for j in range(n - 1, 0, -1):
         if k == 0:
@@ -39,12 +46,14 @@ def heap_sort(a):
             break
         k -= 1
         if k == 0:
-            print(min(a[0], a[j]),max(a[0], a[j]))
+            print(min(a[0], a[j]), max(a[0], a[j]))
             flag = False
             break
         a[0], a[j] = a[j], a[0]
         heapify(a, 0, j)
-heap_sort(a)
+
+
+heap_sort(a)  # 배열삽입
 
 if flag:
     print(-1)
