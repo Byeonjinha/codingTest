5553ca430521550939b70e05cd9802063c137228
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-05-13 09:16:15 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 7.12 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -0,0 +1,8 @@
+def solution(arr, k):
+    answer = []
+    if k % 2 == 0:
+        return list(map(lambda x: x + k , arr))
+    else:
+        return list(map(lambda x: x * k , arr))
+            
+    return answer
