bb481c5582a8973903c1a995fa72d8728c55131b
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-05-18 15:02:18 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -0,0 +1,10 @@
+def solution(myString, pat):
+    myString = list(myString)
+    for i in range(len(myString)):
+        if myString[i] == "A":
+            myString[i] = "B"
+        elif myString[i] == "B":
+            myString[i] = "A"
+    if pat in ''.join(myString):
+        return 1
+    return 0
