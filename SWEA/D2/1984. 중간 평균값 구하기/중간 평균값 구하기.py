5a60af7b5c045ee8d86e9bb8388e80dce7c641ef
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-06-07 11:39:50 +0900

    [D2] Title: 중간 평균값 구하기, Time: 182 ms, Memory: 58,800 KB -BaekjoonHub

---
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
@@ -0,0 +1,6 @@
+T = int(input())
+for i in range(1, T + 1):
+    nums = list(map(int, input().split()))
+    nums.remove(max(nums))
+    nums.remove(min(nums))
+    print("#"+str(i), round(sum(nums)/len(nums)))
