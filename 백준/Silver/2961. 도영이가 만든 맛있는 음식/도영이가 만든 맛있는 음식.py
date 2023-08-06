63b00b911375be7fe531f0b548bbb71270670a4d
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-08-06 15:18:30 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,29 +1,15 @@
-def find_min_difference(N, ingredients):
-    min_difference = float('inf')  # 무한대로 초기화
+import math
+from itertools import combinations
 
-    # 모든 재료의 조합을 확인하기 위한 반복문
-    for i in range(1, N + 1):
-        for combination in itertools.combinations(ingredients, i):
-            s_sum, b_sum = 1, 0  # 요리의 신맛과 쓴맛 초기값 설정
-
-            # 각 조합에 대한 신맛과 쓴맛 계산
-            for s, b in combination:
-                s_sum *= s
-                b_sum += b
-
-            # 차이가 최소인 경우 갱신
-            min_difference = min(min_difference, abs(s_sum - b_sum))
-
-    return min_difference
-
-import itertools
-
-# 입력 처리
-N = int(input())
-ingredients = []
-for _ in range(N):
-    s, b = map(int, input().split())
-    ingredients.append((s, b))
-
-# 결과 출력
-print(find_min_difference(N, ingredients))
+ingredients_count = int(input())
+ingredients = [list(map(int, input().split())) for _ in range(ingredients_count)]
+answer = math.inf
+for i in range(1, ingredients_count + 1):
+    for j in combinations(ingredients, i):
+        S = 1
+        B = 0
+        for k in j:
+            S *= k[0]
+            B += k[1]
+        answer = min(abs(S - B), answer)
+print(answer)
