0a940fc89aad7609ef4e3f1cbd2dd979ad0944eb
Byeon Jinha <87685946+Byeonjinha@users.noreply.github.com>
2023-10-26 23:27:00 +0900

    [Silver II] Title: 연속합, Time: 132 ms, Memory: 121596 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,11 +1,19 @@
 import math
+
 n = int(input())
 num_array = list(map(int, input().split()))
-dp = [0 for _ in range(n)]
 
-dp[0] = num_array[0]
-for index in range(1, len(num_array)):
-    num = num_array[index]
-    dp[index] = max(num, dp[index - 1] + num)
 
-print(max(dp))
+def make_dp_table(num_array):
+    dp = [0 for _ in range(n)]
+    dp[0] = num_array[0]
+
+    for index in range(1, len(num_array)):
+        num = num_array[index]
+        dp[index] = max(num, dp[index - 1] + num)
+    return dp
+
+
+dp_table = make_dp_table(num_array)
+
+print(max(dp_table))
